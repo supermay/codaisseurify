521a0c370d3ec81351255166cbc61e799e81d5ac
supermay <wu_jiamei@outlook.com>
2017-07-08 22:55:09 +0200

    Songs Controller new and create action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,16 @@
 class SongsController < ApplicationController
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params.merge(artist_id: params[:artist_id]))
+    @song.save
+    redirect_to artist_path(params[:artist_id])
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:name,:description,:youtube_link)
+  end 
 end
