304f50c40e8756e00022f427656a18d0a4494eb0
supermay <wu_jiamei@outlook.com>
2017-07-08 19:04:46 +0200

    Testing Associations

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#association test without Shoulda
+  describe "association with song" do
+    let(:artist1) { create :artist }
+    let(:artist2) { create :artist }
+    let(:song1) { create :song, artist: artist1 }
+    let(:song2) { create :song, artist: artist1 }
+    let(:song3) { create :song, artist: artist2 }
+
+    it "has many songs" do
+      expect(artist1.songs).to include(song1,song2)
+    end
+    it "doesn't have songs that are not assigned to it" do
+      expect(artist1.songs).not_to include(song3)
+    end
+  end
+#association test with Shoulda
+  describe "association with song" do
+    it { is_expected.to have_many :songs}
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#association test without Shoulda
+  describe "association with artist" do
+    let(:artist) { create :artist }
+
+    it "belongs to an artist" do
+      song = artist.songs.new
+
+      expect(song.artist).to eq(artist)
+    end
+  end
+#association test with Shoulda
+  describe "association with artist" do
+    it { is_expected.to belong_to :artist}
+  end
 end
