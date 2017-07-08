8a9e5d68af5b304cfec7f0bb0cfe38a8901e5548
supermay <wu_jiamei@outlook.com>
2017-07-08 23:53:14 +0200

    [Root page]is changed into Artist index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'artists#index'
+
   resources :artists
   resources :artists do
     resources :songs, only: [:new,:create,:destroy]
-  end 
+  end
 
 end
