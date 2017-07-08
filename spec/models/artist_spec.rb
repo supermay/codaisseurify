5652edb349c24d31eead46ccdee3672027af366b
supermay <wu_jiamei@outlook.com>
2017-07-08 20:57:22 +0200

    Generate controllers

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Artist, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(50) }
-  end 
+  end
 
 
 
