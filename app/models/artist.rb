363de738d4c595fb5f81c936c65061703812f679
supermay <wu_jiamei@outlook.com>
2017-07-08 20:41:02 +0200

    Cascade Delete

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :songs
+  has_many :songs, dependent: :destroy
   mount_uploader :image, ImageUploader
 
   validates :name, presence: true, length: { maximum: 50}
