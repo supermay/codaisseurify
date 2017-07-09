e1201202e57b9ef64029c5a834dd2f753bfaaef0
supermay <wu_jiamei@outlook.com>
2017-07-09 18:10:23 +0200

    Just in case forgot to submit some changed files again

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,8 @@ class Artist < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :name, presence: true, length: { maximum: 50}
+
+  # def self.order_by_name
+  #   order('name asc')
+  # end
 end
