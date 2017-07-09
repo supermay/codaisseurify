b1d6f7b504e970c2630a683ee7b77e5b98a2edb9
supermay <wu_jiamei@outlook.com>
2017-07-09 12:42:49 +0200

    [new and create action]added to Artist Controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,9 +7,31 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    @artist.image = image_params
+    @artist.save
+    redirect_to artists_path,
+    notice: "New artist has been successfully created!"
+  end
+
   def destroy
     @artist = Artist.find(params[:id])
     @artist.destroy
     redirect_to artists_path
+  end
+
+  private
+
+  def image_params
+    params[:image].present? ? params.require(:image) : nil
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name,:image,:country,:is_band,:website)
   end
 end
