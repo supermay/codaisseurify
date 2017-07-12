415194ab60173aec5d2b84c453260baeca41b0f0
supermay <wu_jiamei@outlook.com>
2017-07-12 20:59:09 +0200

    Remove duplicate artist routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'artists#index'
-
-  resources :artists
+  # 
+  # resources :artists
+  #
   resources :artists do
     resources :songs, only: [:create,:destroy]
   end
