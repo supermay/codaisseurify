e4cde7678adb5f81c8027f7472997e0c49cee716
supermay <wu_jiamei@outlook.com>
2017-07-16 17:16:16 +0200

    deleteAllSongs function feature testing

--- spec/feature/manage_songs_spec.rb
+++ spec/feature/manage_songs_spec.rb
@@ -38,4 +38,30 @@ feature 'Manage songs', js: true do
     expect(page).not_to have_content('I hate javaScript')
 
   end
+
+  scenario 'delete songs with only one click' do
+    artist = Artist.create!(
+    name:"Beatles",
+    remote_image_url: "http://res.cloudinary.com/supermay/image/upload/v1499537718/beatles_mjpoxs.jpg",
+    country: "UK",
+    is_band: true ,
+    website: "http://www.thebeatles.com/")
+
+    song1 = Song.create!(name:"I am serious, I really hate javaScript",
+    youtube_link:"",
+    description:"",
+    artist:artist)
+
+    song2 = Song.create!(name:"Ajax is a headache",
+    youtube_link:"",
+    description:"",
+    artist:artist)
+
+    visit artist_path(artist.id)
+    sleep 3
+    page.execute_script("$('#delete_all_songs').click()")
+    sleep 1
+    expect(page).not_to have_content('I am serious, I really hate javaScript')
+    expect(page).not_to have_content('Ajax is a headache')
+  end
 end
