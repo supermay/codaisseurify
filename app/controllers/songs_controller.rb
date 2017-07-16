489e24f7cdd92a18866af58d7753c3b1292f95ec
supermay <wu_jiamei@outlook.com>
2017-07-16 10:02:28 +0200

    Update Songs controller, create action, react to format

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,16 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.create(song_params.merge(artist_id: params[:artist_id]))
-    @song.save
-    redirect_to artist_path(params[:artist_id])
+
+    respnd_to do |format|
+      if @song.save
+        format.html { redirect_to artist_path(params[:artist_id]), notice: 'Song was created!' }
+        format.json { render json: {}, status: :created, location: @song }
+      else
+        format.html { redirect_to artist_path(params[:artist_id]) }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
